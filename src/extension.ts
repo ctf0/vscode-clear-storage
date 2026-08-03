fb9ae00064b3b832754d5d17cfc7e3c43b42e037
ctf0 <muah003@gmail.com>
2026-08-03 07:50:42 +0300

    003

--- src/extension.ts
+++ src/extension.ts
@@ -23,6 +23,21 @@ async function activate(context) {
             showMsg('global storage cleared')
         }),
     )
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.openWorkSpaceStorage', async() => {
+            const path = context.storagePath.replace('/ctf0.clear-storage', '')
+
+            await openPath(path)
+        }),
+    )
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.openGlobalStorage', async() => {
+            const path = context.globalStoragePath.replace(/globalStorage.*/, 'globalStorage')
+            await openPath(path)
+        }),
+    )
 }
 
 async function listDirectories(rootPath) {
@@ -32,6 +47,11 @@ async function listDirectories(rootPath) {
         .map(async({name: dir}) => {
             await fsExtra.remove(`${rootPath}/${dir}`)
         })
+}
+
+async function openPath(path) {
+    const uri = vscode.Uri.file(path)
+    await vscode.env.openExternal(uri)
 }
 
 function showMsg(msg) {
